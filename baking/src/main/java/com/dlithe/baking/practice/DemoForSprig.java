8bf1a76aceabd6b57f1d46d70472dd101d59dd33
satendra pal <satendrapal9111@gmail.com>
2022-04-25 17:52:23 +0530

    this class is for commit and push

---
+++ baking/src/main/java/com/dlithe/baking/practice/DemoForSprig.java
@@ -0,0 +1,20 @@
+package com.dlithe.baking.practice;
+
+
+/**
+ * <p>this class create for how to commit and push code in git</p>
+ * @author SATENDRA
+ * @version 1.0
+ * @since 25 -04-2022
+ */
+
+public class DemoForSprig {
+
+
+
+    public void test()
+    {
+        System.out.println("hello world");
+    }
+
+}
